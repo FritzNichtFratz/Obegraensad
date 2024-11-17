1e6fb5213aaa2316eb3da519aa5f1dcf0939ae7a
FritzNichtFratz <github_fnf@mailbox.org>
2024-11-17 18:10:23 +0100

    V0.95 Erweiterung um Helligkeitssteuerung
    mit Tastensteuerung

---
+++ Arduino-Sketch/Obegraensad-ESP8266_FnF_V0.95/Parameter.h
@@ -0,0 +1,11 @@
+#define _STASSID "EIGENE_SSD"
+#define _STAPSK  "EIGENES_PW"
+
+// Pinbelegung Wemos D1 mini (clone)
+#define _P_EN  D5  // grau (Pinbezeichnung OE/)
+#define _P_DI  D6  // blau (Pinbezeichnung SDI)
+#define _P_CLK D7  // gelb (Pinbezeichnung CLK)
+#define _P_CLA D1  // braun (Pinbezeichnung LA/)
+#define _P_KEY D2  // grün
+
+#define _MY_NTP_SERVER "pool.ntp.org"
